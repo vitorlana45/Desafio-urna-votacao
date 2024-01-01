236f97b524c14f74e6f640028239c095275cb01e
vitorlana45 <vitoresmerio00@gmail.com>
2024-01-01 14:54:48 -0300

    Conclusao Desafio

---
+++ ExercicioFixacao/src/application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> urna = new TreeMap<>();
+
+
+        System.out.print("Digite o caminho do Arquivo: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int votos = Integer.parseInt(fields[1]);
+
+                if (urna.containsKey(name)) {
+
+                    int votosAtuais = urna.get(name);
+                    urna.put(name, votosAtuais + votos);
+
+                } else {
+                    urna.put(name, votos);
+                }
+                line = br.readLine();
+            }
+            System.out.print("\nCandidatos: ");
+            for(String keys : urna.keySet()){
+                System.out.print(keys + " " );
+            }
+            System.out.println("\n\nContagem de Votos:");
+            for (String key : urna.keySet()) {
+                System.out.println(key + ": " + urna.get(key));
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Desafio-urna-votacao
+# Exercicio Proposto do curso Java [DevSuperior](https://devsuperior.com.br/)
+## Problema em questão !
+* Na contagem de votos de uma eleição, são gerados vários registros
+de votação contendo o nome do candidato e a quantidade de votos
+(formato .csv) que ele obteve em uma urna de votação. Você deve
+fazer um programa para ler os registros de votação a partir de um
+arquivo, e daí gerar um relatório consolidado com os totais de cada
+candidato.
+#
+      Input file example: 
+
+      Alex Blue,15
+      Maria Green,22
+      Bob Brown,21
+      Alex Blue,30
+      Bob Brown,15
+      Maria Green,27
+      Maria Green,22
+      Bob Brown,25
+      Alex Blue,31
+
+      Execution:
+      Enter file full path: c:\temp\in.txt
+      Alex Blue: 76
+      Maria Green: 71
+      Bob Brown: 61
+
+* Alem de entregar o Desafio - Optei por acrecentar novas funções como
+  * Imprimir a lista de Candidatos dentro da urna
+  * imprimir o nome do Vencedor juntamente com seus Votos Somados
+
+---------------------
+# Instruções de uso
+* Fazer o Clone do Repositório.
+* colocar o Arquivo in.txt dentro da pasta de sua escolha.
+  * Ex: C:\\\Temp - dentro da pasta Temp cole o arquivo in.txt
+  * assim ficando C:\\\Temp\\\in.txt
+* Depois inicie a Aplicação e digite o Caminho do Arquivo: C:\\\Temp\\\in.txt
+
